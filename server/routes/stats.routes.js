107b5f1f5157427c9c379838a0f5f9b68f4bb2ed
SaidCarrera <jose.carrera.montesdeoca@udla.edu.ec>
2024-12-02 09:25:59 -0500

    .

--- server/routes/stats.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect, admin } = require('../middleware/auth.middleware');
-const { getStats, updateStats } = require('../controllers/stats.controller');
-
-router.get('/', protect, admin, getStats);
-router.post('/update', protect, admin, updateStats);
-
-module.exports = router;
